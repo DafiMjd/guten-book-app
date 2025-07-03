95d2be85f688d3ab06a9cad9ccec091ed0134ef0
Dafi Majid <dafi.majid@gmail.com>
2025-07-03 10:21:55 +0700

    chore(unit-test): book detail usecase & book usecase

---
+++ shared/domain/test/usecase/book_detail_usecase_test.dart
@@ -0,0 +1,27 @@
+import 'package:data/data.dart';
+import 'package:domain/domain.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../dummy/index.dart';
+
+void main() {
+  late final BookDetailUsecase bookDetailUsecase;
+
+  setUpAll(() {
+    AppGetIt.setup();
+    bookDetailUsecase = BookDetailUsecase();
+  });
+
+  group('Form Selected Id Map', () {
+    test('should map id book and visibility', () {
+      // Arrange
+      final input = BookDummy.booksEven;
+      final expectedOutput = BookDummy.idVisibilityMap;
+
+      // Act
+      final output = bookDetailUsecase.formBookMap(input, BookDummy.savedBookIds);
+      // Assert
+      expect(output, expectedOutput);
+    });
+  });
+}
