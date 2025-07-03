4292752b95a232913e3f3fa7e6d8e57d9c435049
Dafi Majid <dafi.majid@gmail.com>
2025-07-03 15:47:59 +0700

    chore: setup CI/CD to Firebase App Distri

--- apps/guten_book/lib/setup_app.dart
+++ apps/guten_book/lib/setup_app.dart
@@ -3,20 +3,25 @@ import 'dart:async';
 import 'package:data/data.dart';
 import 'package:dependencies/dependencies.dart';
 import 'package:domain/get_it/index.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
 import 'main_app.dart';
 import 'routers/app_router.dart';
 
 class SetupApp {
   static void setup() {
-    runZonedGuarded(() {
+    runZonedGuarded(() async {
       WidgetsFlutterBinding.ensureInitialized();
       AppGetIt.setup(
         additionalDependenciesCallback: () {
           getIt.registerLazySingleton<AppRouter>(AppRouter.new);
           DomainGetIt.setup();
         },
+      );
+      await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
       );
       getIt<Alice>().setNavigatorKey(getIt<AppRouter>().navigatorKey);
       runApp(const MainApp());
